e67f7012c2c4e7ee17267affaaa56dcacb5698d7
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2024-10-21 16:09:51 -0300

    update examples

--- src/stores/pets.ts
+++ src/stores/pets.ts
@@ -8,13 +8,6 @@ export const usePetsStore = create<PetsState>((set) => {
         id: crypto.randomUUID(),
         type: 'dog',
         location: { lat: -33.738186564405446, lng: -61.97131547743373 },
-        condition: 'Healty',
-        description: 'amable',
-      },
-      {
-        id: crypto.randomUUID(),
-        type: 'dog',
-        location: { lat: -33.744538743330196, lng: -61.97723779493861 },
         condition: 'healty',
         description: 'amable',
       },
