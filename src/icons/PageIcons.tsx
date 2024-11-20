cf1f418c664f75839c9a3ccbb0b64e82ed799351
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2024-11-20 15:53:03 -0300

    Add: Page icons

---
+++ src/icons/PageIcons.tsx
@@ -0,0 +1,259 @@
+export function DogIcon() {
+  return (
+    <svg width="32px" height="32px" viewBox="0 0 24 24">
+      <g id="Guides">
+        <g id="_x32_0_px_2_"></g>
+        <g id="_x32_0px"></g>
+        <g id="_x34_0px"></g>
+        <g id="_x34_4_px"></g>
+        <g id="_x34_8px">
+          <g id="_x31_6px"></g>
+          <g id="square_4px">
+            <g id="_x32_8_px">
+              <g id="square_4px_2_"></g>
+              <g id="square_4px_3_"></g>
+              <g id="square_4px_1_"></g>
+              <g id="_x32_4_px_2_"></g>
+              <g id="_x31_2_px"></g>
+            </g>
+          </g>
+        </g>
+        <g id="Icons"></g>
+        <g id="_x32_0_px"></g>
+        <g id="square_6px">
+          <g id="_x31_2_PX"></g>
+        </g>
+        <g id="_x33_6_px">
+          <g id="_x33_2_px">
+            <g id="_x32_8_px_1_">
+              <g id="square_6px_1_"></g>
+              <g id="_x32_0_px_1_">
+                <g id="_x31_2_PX_2_"></g>
+                <g id="_x34_8_px">
+                  <g id="_x32_4_px"></g>
+                  <g id="_x32_4_px_1_"></g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+        <g id="_x32_0_px_3_"></g>
+        <g id="_x32_0_px_4_"></g>
+        <g id="New_Symbol_8">
+          <g id="_x32_4_px_3_"></g>
+        </g>
+      </g>
+      <g id="Artboard"></g>
+      <g id="Free_Icons">
+        <g>
+          <line
+            style={{
+              fill: 'none',
+              stroke: '#747574', // Color gris
+              strokeLinecap: 'round',
+              strokeLinejoin: 'round',
+              strokeMiterlimit: 10,
+            }}
+            x1="7.5"
+            y1="4"
+            x2="7.5"
+            y2="5"
+          />
+          <polyline
+            style={{
+              fill: 'none',
+              stroke: '#747574', // Color gris
+              strokeLinecap: 'round',
+              strokeLinejoin: 'round',
+              strokeMiterlimit: 10,
+            }}
+            points="14,8.5 18.5,18 18.5,23.5"
+          />
+          <polyline
+            style={{
+              fill: 'none',
+              stroke: '#747574', // Color gris
+              strokeLinecap: 'round',
+              strokeLinejoin: 'round',
+              strokeMiterlimit: 10,
+            }}
+            points="10,5 11.5,8.5 17,8.5 13,0.5 6.5,0.5 4.5,4.5 1.5,5.5 1.5,9.5 5.5,9.5 7.5,15.5 5,20.5"
+          />
+          <polyline
+            style={{
+              fill: 'none',
+              stroke: '#747574', // Color gris
+              strokeLinecap: 'round',
+              strokeLinejoin: 'round',
+              strokeMiterlimit: 10,
+            }}
+            points="14.5,16.5 10,16.5 8,20.5 3.5,20.5 2.5,23.5 18.5,23.5"
+          />
+          <polyline
+            style={{
+              fill: 'none',
+              stroke: '#747574', // Color gris
+              strokeLinecap: 'round',
+              strokeLinejoin: 'round',
+              strokeMiterlimit: 10,
+            }}
+            points="18.5,23.5 22.5,19.5 22.5,16 20.5,12"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export function CatIcon() {
+  return (
+    <svg width="32px" height="32px" viewBox="0 0 24 24">
+      <g id="Guides">
+        <g id="_x32_0_px_2_"></g>
+        <g id="_x32_0px"></g>
+        <g id="_x34_0px"></g>
+        <g id="_x34_4_px"></g>
+        <g id="_x34_8px">
+          <g id="_x31_6px"></g>
+          <g id="square_4px">
+            <g id="_x32_8_px">
+              <g id="square_4px_2_"></g>
+              <g id="square_4px_3_"></g>
+              <g id="square_4px_1_"></g>
+              <g id="_x32_4_px_2_"></g>
+              <g id="_x31_2_px"></g>
+            </g>
+          </g>
+        </g>
+        <g id="Icons"></g>
+        <g id="_x32_0_px"></g>
+        <g id="square_6px">
+          <g id="_x31_2_PX"></g>
+        </g>
+        <g id="_x33_6_px">
+          <g id="_x33_2_px">
+            <g id="_x32_8_px_1_">
+              <g id="square_6px_1_"></g>
+              <g id="_x32_0_px_1_">
+                <g id="_x31_2_PX_2_"></g>
+                <g id="_x34_8_px">
+                  <g id="_x32_4_px"></g>
+                  <g id="_x32_4_px_1_"></g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+        <g id="_x32_0_px_3_"></g>
+        <g id="_x32_0_px_4_"></g>
+        <g id="New_Symbol_8">
+          <g id="_x32_4_px_3_"></g>
+        </g>
+      </g>
+      <g id="Artboard"></g>
+      <g id="Free_Icons">
+        <g>
+          {/* Líneas y polígonos con color #747574 */}
+          <line
+            style={{
+              fill: 'none',
+              stroke: '#747574', // Color gris
+              strokeLinecap: 'round',
+              strokeLinejoin: 'round',
+              strokeMiterlimit: 10,
+            }}
+            x1="8.5"
+            y1="4.5"
+            x2="8.5"
+            y2="5"
+          />
+          <line
+            style={{
+              fill: 'none',
+              stroke: '#747574', // Color gris
+              strokeLinecap: 'round',
+              strokeLinejoin: 'round',
+              strokeMiterlimit: 10,
+            }}
+            x1="11.5"
+            y1="4.5"
+            x2="11.5"
+            y2="5"
+          />
+          <polyline
+            style={{
+              fill: 'none',
+              stroke: '#747574', // Color gris
+              strokeLinecap: 'round',
+              strokeLinejoin: 'round',
+              strokeMiterlimit: 10,
+            }}
+            points="7.5,23.5 7.5,8 5.5,5.5 5.5,0.5 9,2.5 11.5,2.5 14.5,0.5 14.5,5.5 12.5,8 20.5,14 22.5,20 20.5,23.5 5,23.5 2.5,21 5,16.5 1.5,13"
+          />
+          <line
+            style={{
+              fill: 'none',
+              stroke: '#747574', // Color gris
+              strokeLinecap: 'round',
+              strokeLinejoin: 'round',
+              strokeMiterlimit: 10,
+            }}
+            x1="12.5"
+            y1="23.5"
+            x2="12.5"
+            y2="13.5"
+          />
+          <line
+            style={{
+              fill: 'none',
+              stroke: '#747574', // Color gris
+              strokeLinecap: 'round',
+              strokeLinejoin: 'round',
+              strokeMiterlimit: 10,
+            }}
+            x1="9.5"
+            y1="23.5"
+            x2="9.5"
+            y2="13.5"
+          />
+          <polyline
+            style={{
+              fill: 'none',
+              stroke: '#747574', // Color gris
+              strokeLinecap: 'round',
+              strokeLinejoin: 'round',
+              strokeMiterlimit: 10,
+            }}
+            points="17.5,17.5 15,17.5 12.5,21.5"
+          />
+          <polyline
+            style={{
+              fill: 'none',
+              stroke: '#747574', // Color gris
+              strokeLinecap: 'round',
+              strokeLinejoin: 'round',
+              strokeMiterlimit: 10,
+            }}
+            points="9.5,7 10,6.5 10.5,7"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export function ResetLocationIcon() {
+  return (
+    <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M19 12C19 15.866 15.866 19 12 19M19 12C19 8.13401 15.866 5 12 5M19 12H21M12 19C8.13401 19 5 15.866 5 12M12 19V21M5 12C5 8.13401 8.13401 5 12 5M5 12H3M12 5V3M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  );
+}
+
+export function AddIcon() {
+  return (
+    <svg fill="#000000" width="32px" height="32px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <path d="M16 0c-8.836 0-16 7.163-16 16s7.163 16 16 16c8.837 0 16-7.163 16-16s-7.163-16-16-16zM16 30.032c-7.72 0-14-6.312-14-14.032s6.28-14 14-14 14 6.28 14 14-6.28 14.032-14 14.032zM23 15h-6v-6c0-0.552-0.448-1-1-1s-1 0.448-1 1v6h-6c-0.552 0-1 0.448-1 1s0.448 1 1 1h6v6c0 0.552 0.448 1 1 1s1-0.448 1-1v-6h6c0.552 0 1-0.448 1-1s-0.448-1-1-1z"></path>
+    </svg>
+  );
+}
