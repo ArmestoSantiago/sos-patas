ac7721e98b672c2cd96717e706239cb90afa1a79
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2024-11-26 17:30:01 -0300

    Solve some problems

--- src/components/MenuPanel/MenuPanel.tsx
+++ src/components/MenuPanel/MenuPanel.tsx
@@ -1,22 +1,26 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { CatIcon, DogIcon, AddIcon, ResetLocationIcon } from '../../icons/PageIcons';
 import { useLocationStore } from '../../stores/location';
 import { usePetsStore } from '../../stores/pets';
 import './MenuPanel.css';
+import { PetsType } from '../../types.d';
 import { ListContainer } from './ListContainer';
 
 export function MenuPanel() {
   const resetLocation = useLocationStore(state => state.resetLocation);
-  const [filter, setFilter] = useState(false);
+  const [filter, setFilter] = useState<false | PetsType>(false);
   const pets = usePetsStore(state => state.pets);
 
-  const handleFilter = (type) => {
-    if (!filter) {
-      setFilter(type);
-    }
+  const handleFilter = (type: PetsType) => {
+    if (filter === type) return setFilter(false);
+    setFilter(type);
   };
 
-  const petsList = pets;
+  const filteredPets = useMemo(() => {
+    return filter
+      ? pets.filter(pet => pet.type === filter)
+      : pets;
+  }, [pets, filter]);
 
   const handleTranslate = () => {
     const menu = document.querySelector('.menu-container');
@@ -38,14 +42,14 @@ export function MenuPanel() {
         <div className='input-container'>
           <input className='location-input' placeholder='Buenos Aires, Cordoba...'>
           </input>
-          <button className='clear-input'>X</button>
+          <button className='clear-input'>✖</button>
         </div>
         <div className='filter-container'>
-          <button onClick={handleFilter} className='filter-button'><DogIcon /></button>
-          <button className='filter-button'><CatIcon /></button>
+          <button className='filter-button' onClick={() => { handleFilter(PetsType.DOG); }}><DogIcon /></button>
+          <button className='filter-button' onClick={() => { handleFilter(PetsType.CAT); }}><CatIcon /></button>
         </div>
         <p className='text'>Resultados</p>
-        <ListContainer petsList={petsList} />
+        <ListContainer pets={filteredPets} />
       </section>
     </>
   );
