cb0902f492956d860037a899d6986904e6c12979
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2024-12-11 18:28:45 -0300

    Refactor component

--- src/components/MenuPanel/SearchInput.tsx
+++ src/components/MenuPanel/SearchInput.tsx
@@ -1,30 +1,75 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { getLocationPrediction } from '../../services/getLocationPrediction';
+import { getGeocode } from '../../services/getGeocode';
+import { useLocationStore } from '../../stores/location';
+import { Location } from '../../types/geocodeTypes';
+import { ThreeDots } from 'react-loader-spinner';
+import { LocationIcon } from '../../icons/PageIcons';
 
 export function InputField() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [optionsOpen, setOptionsOpen] = useState<boolean>(false);
   const [locations, setLocations] = useState<string[]>([]);
+  const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
+  const newLocation = useLocationStore(state => state.newLocation);
 
   const handleClear = () => {
     const input = document.querySelector('.location-input') as HTMLInputElement;
     input.value = '';
     setLocations([]);
+    setOptionsOpen(false);
   };
-
-  console.log(locations, locations.length);
 
   const handleUserSearch = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const predictions = await getLocationPrediction(e.target.value);
-    console.log(predictions);
-    const predictionsArray = predictions.map((prediction: { description: string; }) => prediction.description);
-    setLocations(predictionsArray);
+    setLoading(true);
+    setOptionsOpen(true);
+    if (debounceTimeout.current) {
+      window.clearTimeout(debounceTimeout.current);
+    }
+
+    debounceTimeout.current = setTimeout(async () => {
+      const predictions = await getLocationPrediction(e.target.value);
+      const predictionsArray = predictions.map((prediction: { description: string; }) => prediction.description);
+      setLocations(predictionsArray);
+      setLoading(false);
+    }, 500);
   };
 
+  const handleChangePosition = async (location: string) => {
+    // Take the coords from getGeocode service based on location name, then change map position with thats coords
+    const locationCoords: Location = await getGeocode(location);
+
+    const lat = locationCoords.lat;
+    const lng = locationCoords.lng;
+    console.log(lat, lng);
+    newLocation(lat, lng);
+    setLocations([]);
+    setOptionsOpen(false);
+  };
+
+  const handleClickOutside = (e) => {
+    const isButton = e.target instanceof HTMLButtonElement;
+
+    // Si el clic no es en un botón, limpiar las ubicaciones
+    if (!isButton) {
+      setLocations([]);
+      setOptionsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
   return (
+
     <>
       <div className='input-container'>
         <input
           id="autocomplete"
-          className={`location-input ${locations.length > 0 && 'open-options'}`}
+          className={`location-input ${optionsOpen && 'open-options'}`}
           onChange={(e) => { handleUserSearch(e); }}
           placeholder='Buenos Aires, Cordoba...'>
         </input>
@@ -34,15 +79,36 @@ export function InputField() {
         >✖</button>
       </div>
       {
-        locations.length > 0 &&
+        loading &&
+        <div className='options-container'>
+          {[1, 2, 3].map(button => {
+            return (
+              <button
+                key={button}
+                className='option-button'
+              ><ThreeDots
+                height="24"
+                width="24"
+                color='#444'
+              ></ThreeDots>
+              </button>
+            );
+          })}
+        </div>
+      }
+      {
+        locations.length > 0 && !loading &&
         <div className='options-container'>
           {
             locations.slice(0, 3).map((location, index) => {
               return (
                 <button
+                  disabled={loading}
                   className='option-button'
                   key={index}
-                >{location}
+                  value={location}
+                  onClick={() => { handleChangePosition(location); }}
+                ><LocationIcon /> {location}
                 </button>
               );
             })
