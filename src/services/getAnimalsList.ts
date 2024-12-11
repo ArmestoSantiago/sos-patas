7dba9b70a6db0ab90b02d3fba7b2d005650dab66
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2024-12-11 18:27:07 -0300

    Add comments to clarify the code

--- src/services/getAnimalsList.ts
+++ src/services/getAnimalsList.ts
@@ -1,6 +1,7 @@
 export async function getAnimalsList() {
-  const API_URL = import.meta.env.VITE_APISOSPATAS_URL
+  // Fetch AnimalList from SOSPatas DB
+  const API_URL = import.meta.env.VITE_APISOSPATAS_URL;
   return await fetch(`${API_URL}/animals`)
     .then(res => res.json())
-    .then(data => data)
+    .then(data => data);
 }
--- src/services/getLocationPrediction.ts
+++ src/services/getLocationPrediction.ts
@@ -1,4 +1,5 @@
 export async function getLocationPrediction(input: string) {
+  // Fetch posibles locations name based on user input
   const API_URL = import.meta.env.VITE_APISOSPATAS_URL;
   return await fetch(`${API_URL}/predictions?input=${input}`)
     .then(res => res.json())
