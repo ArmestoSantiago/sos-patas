c7b1ca78acec16eb7d51467ea60ce920b35c0485
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-01-04 21:47:58 -0300

    Bug fix while trying to render loading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import { useLocationStore } from './stores/location';
-import { signIn } from './services/login';
 import { Map } from './components/Map/Map';
 import { MenuPanel } from './components/MenuPanel/MenuPanel';
 import './App.css';
 import { useState } from 'react';
 import { LoadingComponent } from './components/Loading/LoadingComponent';
+import { FirstTimeInstruction } from './components/FirstTimeInstructions/FirstTimeInstructions';
 
 function App() {
   const location = useLocationStore(state => state.location);
   const [loading, setLoading] = useState<boolean>(true);
-  console.log(loading);
-
-  const handleLogIn = async () => {
-    const userInfo = await signIn();
-  };
+  const firstTimeValue = window.localStorage.getItem('first');
+  const firstTime = firstTimeValue ? JSON.parse(firstTimeValue) : false;
 
   return (
     <main className='main-page'>
+      {!firstTime && <FirstTimeInstruction />}
       {loading && <LoadingComponent />}
       <Map
         location={location}
