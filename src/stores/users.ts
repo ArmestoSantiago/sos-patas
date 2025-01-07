2dbee3a8035b95b1c1408a93553d7e2a3b9802a1
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-01-07 16:45:55 -0300

    Create language and text store for managing translations

---
+++ src/stores/users.ts
@@ -0,0 +1,11 @@
+import { create } from 'zustand';
+import { User, UserState } from '../types/usersTypes.d';
+
+export const useUserStore = create<UserState>((set) => {
+  return {
+    userInfo: null,
+    userLogged: false,
+    userLogIn: (user: User) => { set({ userInfo: user, userLogged: true }); },
+    userLogOut: () => { set({ userInfo: null, userLogged: false }); }
+  };
+}); 
