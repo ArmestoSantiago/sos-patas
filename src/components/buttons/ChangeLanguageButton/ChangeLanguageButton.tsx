3fe7215ca31f323e1a3b901222b180c6987d311e
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-01-08 17:37:43 -0300

    update button style

--- src/components/buttons/ChangeLanguageButton/ChangeLanguageButton.tsx
+++ src/components/buttons/ChangeLanguageButton/ChangeLanguageButton.tsx
@@ -4,22 +4,22 @@ import './ChangeLanguageButton.css';
 
 export function ChangeLanguageButton() {
   const textsLang = useTextsStore(state => state.texts.lang);
-  const setTexts = useTextsStore(state => state.setTexts);
+  const setLanguage = useTextsStore(state => state.setLanguage);
 
   const handleToggleLanguage = () => {
     if (textsLang === ValidLanguages.ES) {
-      setTexts(ValidLanguages.EN);
+      setLanguage(ValidLanguages.EN);
     };
     if (textsLang === ValidLanguages.EN) {
-      setTexts(ValidLanguages.ES);
+      setLanguage(ValidLanguages.ES);
     };
   };
 
   return (
-    <div className="flex-cradle">
-      <div className="slider-cradle">
-        <div className="option-1">EN</div>
+    <div onClick={handleToggleLanguage} className="flex-cradle">
+      <div className={`slider-cradle ${textsLang === ValidLanguages.ES && 'is-transitioned'}`}>
         <div className="option-2">ES</div>
+        <div className="option-1">EN</div>
         <div className="handle"></div>
       </div>
     </div>
