c85c947f8d21586945691d6584f013787cd98910
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-01-08 17:24:55 -0300

    delete some innecessary code

--- src/components/buttons/ChangeLanguageButton/ChangeLanguageButton.tsx
+++ src/components/buttons/ChangeLanguageButton/ChangeLanguageButton.tsx
@@ -8,21 +8,20 @@ export function ChangeLanguageButton() {
 
   const handleToggleLanguage = () => {
     if (textsLang === ValidLanguages.ES) {
-      console.log('Porno');
       setTexts(ValidLanguages.EN);
     };
     if (textsLang === ValidLanguages.EN) {
-      console.log('Porn');
       setTexts(ValidLanguages.ES);
     };
   };
 
   return (
-    <div className="switch">
-      <input onChange={handleToggleLanguage} id="language-toggle" className="check-toggle check-toggle-round-flat" type="checkbox" />
-      <label htmlFor="language-toggle"></label>
-      <span id="ES" className={textsLang === 'ES' ? 'on' : 'off'}>ES</span>
-      <span id="EN" className={textsLang === 'EN' ? 'on' : 'off'}>EN</span>
+    <div className="flex-cradle">
+      <div className="slider-cradle">
+        <div className="option-1">EN</div>
+        <div className="option-2">ES</div>
+        <div className="handle"></div>
+      </div>
     </div>
   );
 }
