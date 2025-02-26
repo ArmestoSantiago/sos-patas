0adc440e83ee5b25f385d5d64a20f9e708775fb5
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-02-25 21:09:11 -0300

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest/config" /> 
+import path from 'node:path';
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
@@ -8,5 +9,17 @@ export default defineConfig({
   test: {
     environment: 'happy-dom',
     setupFiles: ['./tests/App.test.tsx']
-  }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@stores': path.resolve(__dirname, 'src/stores'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@types': path.resolve(__dirname, 'src/types'),
+      '@icons': path.resolve(__dirname, 'src/icons'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+    },
+  },
 });
