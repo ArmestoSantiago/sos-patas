746f54f690eb1f7d5708cb17bea64ec8a5c5f9f1
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-02-25 21:10:47 -0300

    Update const

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,13 +1,17 @@
-import { GOOGLE_MAPS_ID } from '../config';
+import { GOOGLE_MAPS_ID } from '@/config';
 
 export const CONST = {
   defaultLocation: { lat: -33.860664, lng: 151.208138 }  // default map position 
 };
 
-export const MAP_CONFIGURATION = {
+export const MAP_CONFIGURATION: google.maps.MapOptions = {
   mapId: GOOGLE_MAPS_ID,
-  defaultZoom: 15,
+  zoom: 15,
   disableDefaultUI: true,
-  fullscreenOption: false,
-  daggable: true
+  // fullscreenControlOptions: false,
+  draggable: true,
+  clickableIcons: false,
 };
+
+const petsPostedLocalValue = window.localStorage.getItem('animalList');
+export const petsPostedLocal = petsPostedLocalValue ? JSON.parse(petsPostedLocalValue) : [];
