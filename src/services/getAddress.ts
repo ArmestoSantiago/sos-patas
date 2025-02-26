ff40254ea62fcab5cba46efab08ce1828374c1aa
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-02-25 21:11:02 -0300

    Refactor reverse geolocation function

--- src/services/getAddress.ts
+++ src/services/getAddress.ts
@@ -1,5 +1,5 @@
-import { GOOGLE_GEOCODE_APIKEY, GOOGLE_GEOCODE_URL } from '../config';
-import { Location } from '../types/locationTypes';
+import { GOOGLE_GEOCODE_APIKEY, GOOGLE_GEOCODE_URL } from '@/config';
+import { Location } from '@/types/locationTypes';
 
 export const getAddress = async ({ lat, lng }: Location) => {
   // Transfor from coords to address name
@@ -7,5 +7,9 @@ export const getAddress = async ({ lat, lng }: Location) => {
   const GEOCODE_URL = `${GOOGLE_GEOCODE_URL}/json?latlng=${lat},${lng}&key=${GOOGLE_GEOCODE_APIKEY}`;
   return fetch(GEOCODE_URL)
     .then(res => res.json())
-    .then(addressInfo => addressInfo);
+    .then(addressInfo => {
+      const formateAddress = addressInfo.results[0].formatted_address.split(',')[0];
+      return formateAddress;
+    })
+    .catch(err => 'Not found');
 };
