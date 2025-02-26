21707d08e9bf6b1a461148b4c53841dd6f837325
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-02-25 21:41:26 -0300

    Update location store

--- src/stores/location.ts
+++ src/stores/location.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
-import { LocationState, MapType } from '../types/locationTypes.d';
-import { getLocation } from '../services/getLocation';
+import { LocationState, MapType } from '@/types/locationTypes.d';
+import { getLocation } from '@services/getLocation';
 
 const initialState: MapType = {
   location: await getLocation().then((location) => location),
@@ -10,6 +10,19 @@ export const useLocationStore = create<LocationState>((set, get) => {
   return {
     ...initialState,
     draggable: false,
+    toAddAnimal: false,
+    locationNewAnimal: undefined,
+    openForm: false,
+    setOpenForm: () => {
+      const openModal = get().openForm;
+      set({ openForm: !openModal });
+    },
+    setLocationNewAnimal: (lat: number, lng: number) => {
+      set({ locationNewAnimal: { lat: lat, lng: lng } });
+    },
+    setToAddAnimal: (type) => {
+      set({ toAddAnimal: type });
+    },
     newLocation: (lat: number, lng: number) => set({ location: { lat: lat, lng: lng } }),
     setDraggable: () => {
       const isDraggable = get().draggable;
