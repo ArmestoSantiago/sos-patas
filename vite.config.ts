95537b2cdeba15df36f66e4d9e87a2b19935e351
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-03-04 19:25:50 -0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  preview: {
+    port: 5173
+  },
   plugins: [react()],
   test: {
     environment: 'happy-dom',
