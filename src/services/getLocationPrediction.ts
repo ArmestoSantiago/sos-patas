27d0e2e6d108eae03ae4f13948891cb62bb83bfc
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-03-18 11:56:14 -0300

    Fix getPredictions errors

--- src/services/getLocationPrediction.ts
+++ src/services/getLocationPrediction.ts
@@ -1,7 +1,17 @@
+import { API_URL } from '@/config';
+
 export const getLocationPrediction = async (input: string) => {
   // Fetch posibles locations name based on user input
-  const API_URL = import.meta.env.VITE_APISOSPATAS_URL;
-  return await fetch(`${API_URL}/predictions?input=${input}`)
-    .then(res => res.json())
-    .then(data => data);
+
+  const fullURL = `${API_URL}/predictions?input=${input}`;
+  try {
+    const res = await fetch(fullURL);
+    if (!res.ok) throw Error();
+    const data = await res.json();
+    return data.predictions;
+
+  } catch (err) {
+    return false;
+  }
+
 };
