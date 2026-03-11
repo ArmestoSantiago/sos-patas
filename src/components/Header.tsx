fff0f4c39b9561029fb3200babb20c64536032f9
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2026-03-11 16:02:04 -0300

    Update Style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export function Header({ title }: { title?: string; }) {
   return (
-    <header className="border-b border-stone-200 h-30">
+    <header className="border-b border-stone-200 h-20 bg-white sticky top-0 z-10">
       <div className="max-w-2xl mx-auto">
         <h1 className="text-main text-4xl font-bold">{title}</h1>
         <span>Red solidaria animal</span>
