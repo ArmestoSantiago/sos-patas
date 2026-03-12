3e1cb374c29aaac8d0aeebb5bc0e7097dd18ff94
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2026-03-12 11:58:44 -0300

    refactor: API calls

--- src/services/getPostedByUser.ts
+++ src/services/getPostedByUser.ts
@@ -2,7 +2,7 @@ import { API_URL } from '@/config';
 
 export const getPostedByUser = async (userId: string) => {
 
-  const response = await fetch(`${API_URL}/users?userid=${userId}`);
+  const response = await fetch(`${API_URL}/users/${userId}/posts`);
   const postedByUser = await response.json();
   return postedByUser;
 };
--- src/services/login.ts
+++ src/services/login.ts
@@ -1,7 +1,9 @@
 import { initializeApp } from 'firebase/app';
+import { API_URL, SOSPATAS_API_KEY } from '@/config';
 import { getAuth } from 'firebase/auth';
 import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { FIREBASE_APIKEY as apiKey, FIREBASE_APP_ID as appId, FIREBASE_AUTH_DOMAIN as authDomain, FIREBASE_MESSAGIN_SENDER_ID as messagingSenderId, FIREBASE_PROJECT_ID as projectId, FIREBASE_STORAGE_BUCKET as storageBucket } from '@/config';
+import { getUserById } from './getUsers';
 
 export const firebaseConfig = {
   apiKey,
@@ -27,15 +29,53 @@ export const signOutFn = async () => {
 
 export const signIn = async () => {
   return signInWithPopup(auth, provider)
-    .then((result) => {
+    .then(async (result) => {
       const { displayName, uid, photoURL } = result.user;
       const user = {
         name: displayName ?? 'Usuario',
         id: uid,
         photo: photoURL ?? 'public/avatar.png'
       };
+
+      const userExists = await checkUsersExistence(user.id);
+      console.log(userExists);
+
+      if (!userExists) {
+        await registNewUser(user);
+
+      }
+
       return user;
     }).catch((error) => {
       console.log(error);
     });
+};
+
+const registNewUser = async (data: { name: string, id: string, photo: string; }) => {
+  const { name: displayName, id, photo: photoURL } = data;
+
+  const payload = {
+    displayName,
+    id,
+    photoURL
+  };
+
+  try {
+    const registedUser = await fetch(`${API_URL}/users/`, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: {
+        'x-api-key': SOSPATAS_API_KEY,
+        'Content-Type': 'application/json'
+      }
+    });
+    const data2 = await registedUser.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const checkUsersExistence = async (id: string) => {
+  const user = await getUserById(id);
+  return user.length > 0 ? true : false;
 };
