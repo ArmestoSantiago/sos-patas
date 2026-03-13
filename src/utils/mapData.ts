98b7a4caf2eb58a8934b8f423a9ed2b2ca32cabc
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2026-03-13 11:13:49 -0300

    fix: wrong variable name

--- src/utils/mapData.ts
+++ src/utils/mapData.ts
@@ -3,7 +3,7 @@ import { PetsCondition, PetsType } from '@/types/petsTypes.d';
 export const mapData = (data: React.FormEvent<HTMLFormElement>) => {
   const elements = data.currentTarget.elements as FormElements;
   const type = elements.petType.value === PetsType.DOG ? PetsType.DOG : PetsType.CAT;
-  const condition = elements.petCondition.value === PetsCondition.HEALTHY ? PetsCondition.HEALTHY : elements.petCondition.value === PetsCondition.WOUNDED ? PetsCondition.WOUNDED : PetsCondition.CRTICIAL;
+  const condition = elements.petCondition.value === PetsCondition.HEALTHY ? PetsCondition.HEALTHY : elements.petCondition.value === PetsCondition.WOUNDED ? PetsCondition.WOUNDED : PetsCondition.CRITICAL;
 
   const mappedData = {
     type,
