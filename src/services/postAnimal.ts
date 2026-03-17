19500c38f101d81892965f4c3abddae7affc40fc
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2026-03-16 22:12:12 -0300

    Refactor: Change the way post animal is done

--- src/services/postAnimal.ts
+++ src/services/postAnimal.ts
@@ -1,48 +1,32 @@
 import { API_URL, SOSPATAS_API_KEY } from '@/config';
-import { PetsInformation, PetsPostData, PetsType } from '@/types/petsTypes.d';
+import { PetsCondition, PetsInformation, PetsSituation, PetsType } from '@/types/petsTypes.d';
 import { User } from '@/types/usersTypes.d';
-import { getAddress } from './getAddress';
-import { petsPostedLocal } from '@/const/const';
 import { getPostedByUser } from './getPostedByUser';
 import { uploadImage } from './uploadImage';
 
-const dogExample = './dog-example.jpg';
-const catExample = './cat-example.jpg';
-
-export const postAnimal = async ({ data, user, image }: PostAnimalParams) => {
-
-  const address: string = await getAddress({ lat: data.lat, lng: data.lng });
-
-  if (!user) {
-    // if there is no user the information will be asigned by default
-    const toPostData: PetsInformation = {
-      user_id: 'guest',
-      id: crypto.randomUUID(),
-      ...data,
-      address,
-      imgSrc: data.type === PetsType.DOG ? dogExample : catExample
-    };
-
-    const toPostDataGuest = petsPostedLocal.concat(toPostData);
-    window.localStorage.setItem('animalList', JSON.stringify(toPostDataGuest));
-    return { posted: true };
-  }
+export const postAnimal = async (data: PostAnimalParams) => {
+  const { userInfo: user, image, address, description, name, type, situation, condition, lat, lng } = data;
 
   try {
     const postedByUser: PetsInformation[] = await getPostedByUser(user.id);
 
-    if (postedByUser.length >= 3) {
+    if (postedByUser.length >= 6) {
+      console.log('OH OH');
       throw ({ code: 429, posted: false });
     }
 
-    const blob = new Blob([image]);
-    const imageURL = await uploadImage(blob);
+    const imageURL = await uploadImage(image);
 
-    const toPostData: PetsInformation = {
-      ...data,
+    const toPostData = {
       user_id: user.id,
+      animal_name: name,
+      type,
+      situation,
+      description,
+      condition,
+      lat, lng,
       address,
-      imgSrc: imageURL,
+      imgSrc: imageURL
     };
 
     const response = await fetch(`${API_URL}/animals`, {
@@ -65,11 +49,19 @@ export const postAnimal = async ({ data, user, image }: PostAnimalParams) => {
       posted: false,
       code: error.code
     };
-  }
-
+  };
 };
+
 interface PostAnimalParams {
-  data: PetsPostData;
-  user: null | User;
+  userInfo: User;
+  address: string,
+  description: string,
+  name: string;
+  type: PetsType;
+  situation: PetsSituation;
   image: File;
-}
+  condition: PetsCondition,
+  lat: number,
+  lng: number;
+};
+
