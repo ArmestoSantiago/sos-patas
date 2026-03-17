4815121c61d7968505a9e59c276630750051e320
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2026-03-17 12:01:43 -0300

    add tick icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -105,3 +105,11 @@ export const HeartIcon = () => {
     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-heart inline mr-2" aria-hidden="true" ><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></svg>
   );
 };
+
+export const TickIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="22px" height="22px" viewBox="0 0 24 24" fill="none" >
+      <path d="M4.89163 13.2687L9.16582 17.5427L18.7085 8" stroke="#fff" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg >
+  );
+};
