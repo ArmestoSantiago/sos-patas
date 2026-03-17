d90a43672cfb350fca16985f86184ddce28fe075
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2026-03-17 00:24:34 -0300

    refactor component

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,15 +1,31 @@
-import { GoogleLoginButton } from '@/components/buttons/GoogleLoginButton/GoogleLoginButton';
 import { Header } from '@/components/Header';
+import { LogInComponent } from '@/components/LogInComponent';
 import { NavMenu } from '@/components/NavMenu';
+import { UserProfile } from '@/components/UserProfile';
+import { getPostedByUser } from '@/services/getPostedByUser';
+import { useUserStore } from '@/stores/users';
+import { useEffect, useState } from 'react';
 
 export function Profile() {
+  const { userInfo } = useUserStore();
+  const [posts, setPosts] = useState([]);
+  console.log(posts);
+
+  useEffect(() => {
+    if (!userInfo) return;
+    const getPosts = async () => {
+      const id = userInfo.id;
+      const posts = await getPostedByUser(id);
+      setPosts(posts);
+    };
+
+    getPosts();
+  }, []);
   return (
     <>
       <Header title="Profile" />
-      <div className="max-w-160 w-full mx-auto text-center my-6 pb-20 px-4">
-
-        <GoogleLoginButton method={true} />
-      </div>
+      {!userInfo && <LogInComponent />}
+      {userInfo && <UserProfile posts={posts} />}
       <NavMenu />
     </>
   );
