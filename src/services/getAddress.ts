02177d74ef7ea011522914f9b286ed94a656cba3
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2026-05-18 12:26:20 -0300

    refactor: improve geocode error handling

--- src/services/getAddress.ts
+++ src/services/getAddress.ts
@@ -5,12 +5,18 @@ export const getAddress = async ({ lat, lng }: Location) => {
   // Transfor from coords to address name
 
   const GEOCODE_URL = `${GOOGLE_GEOCODE_URL}/json?latlng=${lat},${lng}&key=${GOOGLE_GEOCODE_APIKEY}`;
-  return fetch(GEOCODE_URL)
-    .then(res => res.json())
-    .then(addressInfo => {
-      console.log(addressInfo);
-      const formateAddress = addressInfo.results[0].formatted_address.split(',')[0];
-      return formateAddress;
-    })
-    .catch(() => null);
+
+  try {
+    const res = await fetch(GEOCODE_URL);
+    if (!res.ok) throw Error();
+    const addressInfo = await res.json();
+    const firstResult = addressInfo.results?.[0];
+    if (!firstResult) throw Error();
+    const formateAddress = firstResult?.formatted_address.split(',')[0]; 1;
+    return formateAddress;
+  } catch (err) {
+    return null;
+  }
+
 };
+
